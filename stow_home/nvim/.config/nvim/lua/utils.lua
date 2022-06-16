cc360e98ea12111ef7126de41c781fa9ec514b7d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-06-16 17:33:08 +0300

    [Enh](Utils): Added prequire that will throw error if the plugin is missing.

--- stow_home/nvim/.config/nvim/lua/utils.lua
+++ stow_home/nvim/.config/nvim/lua/utils.lua
@@ -1,8 +1,15 @@
 local M = {}
 
-M.prequire = function (plugin_name)
+local function prequire(plugin_name)
   local plugin_loading_error_handler = function(error)
-    vim.notify('Error in loading plugin ' .. plugin_name .. '!');
+    local nvim_notify_is_available, nvim_notify = pcall(require, 'notify');
+
+    local notify = vim.notify;
+    if nvim_notify_is_available then
+      notify = nvim_notify;
+    end
+
+    notify('Error in loading plugin ' .. plugin_name .. '!');
   end
 
   local status_ok, plugin = xpcall(require, plugin_loading_error_handler, plugin_name);
@@ -14,5 +21,14 @@ M.prequire = function (plugin_name)
   return status_ok, plugin;
 end
 
-return M;
+-- Shortcut for printing variables in a meaningless way: showing contents of a
+--   table via vim.inspect. Used log as console.log in js works pretty the same
+--   way.
+local function log(data)
+  print(vim.inspect(data))
+end
+
+return {
+  prequire = prequire,
+};
 
