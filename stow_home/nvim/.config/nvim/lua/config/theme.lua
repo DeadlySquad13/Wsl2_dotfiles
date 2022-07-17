c65ac959cbceb2c3d331414513f52ba2e19f3301
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-07-17 19:46:31 +0300

    [Enh](LoadColorscheme): Added backup and fallback values for theme loading.

--- stow_home/nvim/.config/nvim/lua/config/theme.lua
+++ stow_home/nvim/.config/nvim/lua/config/theme.lua
@@ -1,11 +1,5 @@
-local COLORSCHEME_NAME = 'deadly-gruv'
-
-local function load_coloscheme(colorscheme_name)
-  local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. COLORSCHEME_NAME)
-
-  if not status_ok then
-    return notify('colorscheme ' .. colorscheme_name .. ' not found!')
-  end
-end
-
-load_coloscheme(COLORSCHEME_NAME)
+return {
+  COLORSCHEME_NAME = 'deadly-gruv',
+  BACKUP_COLORSCHEME_NAME = 'gruvbox-material',
+  FALLBACK_COLORSCHEME_NAME = 'shine',
+}
