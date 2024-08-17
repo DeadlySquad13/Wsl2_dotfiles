621829a7ba4f11289def5d314846be1306dcffc0
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-17 10:55:59 +0000

    [Enh](Ranger): Added new commands.

--- stow_home/ranger/.config/ranger/commands.py
+++ stow_home/ranger/.config/ranger/commands.py
@@ -1,62 +1,85 @@
-# This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
-
-from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
+import subprocess
 import os
 
-# You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
+from ranger.ext.get_executables import get_executables
 
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
+# # Broot integration.
+def show_error_in_console(msg, fm):
+    fm.notify(msg, bad=True)
 
-    A sample command for demonstration purposes that opens a file in an editor.
-    """
 
-    # The execute method is called when you run this command in ranger.
+def navigate_path(fm, selected):
+    if not selected:
+        return
+
+    selected = os.path.abspath(selected)
+    if os.path.isdir(selected):
+        fm.cd(selected)
+    elif os.path.isfile(selected):
+        fm.select_file(selected)
+    else:
+        show_error_in_console(f"Neither directory nor file: {selected}", fm)
+        return
+
+
+def run_and_cd(cmd, fm):
+    proc = fm.execute_command(cmd, text=False, stdout=subprocess.PIPE)
+    stdout, _ = proc.communicate()
+    if proc.returncode != 0:
+        show_error_in_console(f"Bad process exit code: {proc.returncode}", fm)
+        return
+
+    navigate_path(fm, stdout.strip())
+
+
+class broot_select(Command):  # noqa
     def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
-        else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
-
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
-
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
+        if "broot" not in get_executables():
+            show_error_in_console("Could not find broot", self.fm)
             return
 
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
+        run_and_cd("broot", self.fm)
 
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
+
+# Copy the content of image file and text file with xclip
+from ranger.container.file import File
+
+
+class YankContent(Command):
+    """
+    Copy the content of image file and text file with xclip
+    """
+
+    def execute(self):
+        if 'xclip' not in get_executables():
+            self.fm.notify('xclip is not found.', bad=True)
+            return
+
+        arg = self.rest(1)
+        if arg:
+            if not os.path.isfile(arg):
+                self.fm.notify('{} is not a file.'.format(arg))
+                return
+            file = File(arg)
+        else:
+            file = self.fm.thisfile
+            if not file.is_file:
+                self.fm.notify('{} is not a file.'.format(file.relative_path))
+                return
+
+        relative_path = file.relative_path
+        cmd = ['xclip', '-selection', 'clipboard']
+        if not file.is_binary():
+            with open(file.path, 'rb') as fd:
+                subprocess.check_call(cmd, stdin=fd)
+        elif file.image:
+            cmd += ['-t', file.mimetype, file.path]
+            subprocess.check_call(cmd)
+            self.fm.notify('Content of {} is copied to x clipboard'.format(relative_path))
+        else:
+            self.fm.notify('{} is not an image file or a text file.'.format(relative_path))
+
     def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
         return self._tab_directory_content()
